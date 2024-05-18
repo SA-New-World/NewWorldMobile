43c242d4886eb56698e5f2441314e79e8046a7c0
bastien <bastien.raoult1pro@gmail.com>
2024-05-18 12:27:33 +0200

    création de la page de connection/inscription

---
+++ lib/view/pages/login_signup_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+/// Création d'une page de connection/inscription
+class SignInSignUpPage extends StatelessWidget {
+  const SignInSignUpPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
