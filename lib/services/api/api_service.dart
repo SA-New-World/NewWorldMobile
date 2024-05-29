05cdb34ea72220ef706c19bb20ed90c6527b994a
bastien <bastien.raoult1pro@gmail.com>
2024-05-29 09:12:39 +0200

    Ajout des images produits

--- lib/services/api/api_service.dart
+++ lib/services/api/api_service.dart
@@ -136,8 +136,6 @@ class ApiService {
         Map data = response.data;
         List<dynamic> results = data["hydra:member"];
 
-        print('Page $page: récupéré ${results.length} produits');
-
         // Si la réponse ne contient pas de produits, on arrête la boucle
         if (results.isEmpty) {
           hasMorePages = false;
@@ -148,7 +146,7 @@ class ApiService {
               id: json['id'],
               name: json['name'],
               description: json['description'],
-              price: json['price'],
+              price: json['price'].toDouble(),
             );
             allProducts.add(product);
           }
@@ -156,7 +154,6 @@ class ApiService {
           page++;
         }
       } catch (e) {
-         print('Erreur lors de la récupération des produits : $e');
         hasMorePages = false; // Arrêter la boucle en cas d'erreur
       }
     }
---
+++ lib/view/pages/product_detail_page.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:new_world_mobile/view/components/product_card.dart';
+import '../../models/product.dart';
+import '../../services/api/api_service.dart';
+
+// La classe ProductPage représente une page qui affiche une liste de produits.
+// Elle est Stateless, ce qui signifie qu'elle ne conserve pas d'état mutable elle-même.
+class ProductDetailPage extends StatelessWidget {
+  ProductDetailPage({super.key}); // Constructeur de la classe ProductPage.
+  static const routeName = '/product'; // Nom de la route pour la navigation.
+
+  // Liste pour stocker les produits récupérés depuis le service API.
+  List<Product> card = [];
+
+  // Fonction asynchrone pour récupérer les produits depuis l'API.
+  Future<void> setProducer() async {
+    ApiService service =
+        ApiService(); // Création d'une instance du service API.
+    List<Product> products = await service
+        .getAllProducts(); // Récupération des produits depuis l'API.
+    card = products; // Mise à jour de la liste des produits.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Utilisation de FutureBuilder pour gérer les opérations asynchrones.
+      future: setProducer(), // Le futur à attendre (récupération des produits).
+      builder: (context, AsyncSnapshot snapshot) {
+        // Si l'application est en cours de chargement
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          // Affiche un indicateur de progression circulaire au centre de l'écran.
+          return const Center(child: CircularProgressIndicator());
+          // Si une erreur s'est produite pendant la récupération des données
+        } else if (snapshot.hasError) {
+          // Affiche l'erreur
+          return Center(child: Text('Error: ${snapshot.error}'));
+          // Si la récupération des données s'est terminée avec succès
+        } else {
+          // Construit une liste de cartes de produits
+          return ListView.builder(
+            itemCount: card.length, // Nombre d'éléments dans la liste
+            itemBuilder: (context, index) {
+              // Construit chaque élément de la liste (une carte de produit)
+              return Card(
+                child: ProductCard(
+                  // Les données à afficher sur la carte de produit
+                  cardName: card[index].name, // Nom du produit
+                  cardPrice: card[index].price, // Prix du produit
+                  cardImg:
+                      'images/${card[index].name}.jpg', // Image du produit (fixe ici)
+                ),
+              );
+            },
+          );
+        }
+      },
+    );
+  }
+}
