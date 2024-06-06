a953a65b28e165630af21225c6ba36c6a4f59cc8
bastien <bastien.raoult1pro@gmail.com>
2024-06-06 08:18:23 +0200

    ajout

--- lib/view/pages/product_detail_page.dart
+++ lib/view/pages/product_detail_page.dart
@@ -24,8 +24,7 @@ class ProductDetailPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Center(
-              child: Image.asset('images/${product.name.toLowerCase()}.jpg',
-                  height: 200),
+              child: Image.asset('images/${product.name}.jpg', height: 200),
             ),
             const SizedBox(height: 20),
             Text(
